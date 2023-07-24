d95729aea6fc98287763cb1808c5dea9bac2d52f
juan <109624079+juannotdev@users.noreply.github.com>
2023-07-24 01:04:23 -0400

    server.lua - Added chat filter

--- ND_Chat/source/server.lua
+++ ND_Chat/source/server.lua
@@ -2,49 +2,69 @@ NDCore = exports["ND_Core"]:GetCoreObject()
 NDCore.Functions.VersionChecker("ND_Chat", GetCurrentResourceName(), "https://github.com/Andyyy7666/ND_Framework", "https://raw.githubusercontent.com/Andyyy7666/ND_Framework/main/ND_Chat/fxmanifest.lua")
 
 if config["/me"] then
-    RegisterCommand("me", function(source, args, rawCommand, text)
+    RegisterCommand("me", function(source, args, message, text)
         local player = source
         local players = NDCore.Functions.GetPlayers()
         local playerCoords = GetEntityCoords(GetPlayerPed(player))
         for serverPlayer, _ in pairs(players) do
             local targetCoords = GetEntityCoords(GetPlayerPed(serverPlayer))
             if (#(playerCoords - targetCoords) < 20.0) then
+                local function_check, msg = chatblacklist(message)
+                if not function_check then
                 TriggerClientEvent("chat:addMessage", serverPlayer, {
                     color = {255, 0, 0},
                     args = {"^*^3[ME] " .. players[player].firstName .. " " .. players[player].lastName .. " [" .. players[player].job .. "] (#" .. player .. ")", table.concat(args, " ")}
                 })
+                else
+                    TriggerClientEvent('chatMessage', player, string.format(config.filtermessage, msg))
+                end
             end
         end
     end, false)
 end
 
 if config['/gme'] then
-    RegisterCommand('gme', function(source, args)
+    RegisterCommand('gme', function(source, args, message)
         local player = source
         local source = source
         local character = NDCore.Functions.GetPlayer(source)
-
-        TriggerClientEvent('chat:addMessage', -1, { args = {"^*^6[GME] " .. character.firstName .. " " .. character.lastName .. " [" .. character.job .. "] (#" .. player .. ")", table.concat(args, " ")}})
+        
+        local function_check, msg = chatblacklist(message)
+        if not function_check then
+            TriggerClientEvent('chat:addMessage', -1, { args = {"^*^6[GME] " .. character.firstName .. " " .. character.lastName .. " [" .. character.job .. "] (#" .. player .. ")", table.concat(args, " ")}})
+        else
+            TriggerClientEvent('chatMessage', player, string.format(config.filtermessage, msg))
+        end
     end, false)
 end
 
 if config['/twt'] then
-    RegisterCommand('twt', function(source, args)
+    RegisterCommand('twt', function(source, args, message)
         local player = source
         local source = source
         local character = NDCore.Functions.GetPlayer(source)
 
-        TriggerClientEvent('chat:addMessage', -1, { args = {"^*^4[Twitter] @" .. character.data.twtName .. " (#" .. player .. ")", table.concat(args, " ")}})
+        local function_check, msg = chatblacklist(message)
+        if not function_check then
+            TriggerClientEvent('chat:addMessage', -1, { args = {"^*^4[Twitter] @" .. character.data.twtName .. " (#" .. player .. ")", table.concat(args, " ")}})
+        else
+            TriggerClientEvent('chatMessage', player, string.format(config.filtermessage, msg))
+        end
     end, false)
 end
 
 if config['/ooc'] then
-    RegisterCommand('ooc', function(source, args)
+    RegisterCommand('ooc', function(source, args, message)
         local player = source
         local source = source
         local character = NDCore.Functions.GetPlayer(source)
 
-        TriggerClientEvent('chat:addMessage', -1, { args = {"^*^2[OOC] " .. GetPlayerName(source) .. " [" .. character.job .. "] (#" .. player .. ")", table.concat(args, " ")}})
+        local function_check, msg = chatblacklist(message)
+        if not function_check then
+            TriggerClientEvent('chat:addMessage', -1, { args = {"^*^2[OOC] " .. GetPlayerName(source) .. " [" .. character.job .. "] (#" .. player .. ")", table.concat(args, " ")}})
+        else
+            TriggerClientEvent('chatMessage', player, string.format(config.filtermessage, msg))
+        end
     end, false)
 
 function hasDarkWebPermission(player, players, args)
@@ -71,17 +91,48 @@ function hasDarkWebPermission(player, players, args)
 end
 
 if config["/darkweb"].enabled then
-    RegisterCommand("darkweb", function(source, args, rawCommand)
+    RegisterCommand("darkweb", function(source, args, rawCommand, message)
         local player = source
         local players = NDCore.Functions.GetPlayers()
-        hasDarkWebPermission(player, players, args)
+        local function_check, msg = chatblacklist(message)
+        if not function_check then
+            hasDarkWebPermission(player, players, args)
+        else
+            TriggerClientEvent('chatMessage', player, string.format(config.filtermessage, msg))
+        end
     end, false)
 end
 
 if config["/911"].enabled then
-    RegisterCommand("911", function(source, args, rawCommand)
+    RegisterCommand("911", function(source, args, rawCommand, message)
         local player = source
-        TriggerClientEvent("ND_Chat:911", -1, GetEntityCoords(GetPlayerPed(player)), table.concat(args, " "))
+        local function_check, msg = chatblacklist(message)
+        if not function_check then
+            TriggerClientEvent("ND_Chat:911", -1, GetEntityCoords(GetPlayerPed(player)), table.concat(args, " "))
+        else
+            TriggerClientEvent('chatMessage', player, string.format(config.filtermessage, msg))
+        end
     end, false)
 end
+end
+
+
+------- Blacklist
+
+function chatblacklist(str)
+    local blacklist = false;
+    local word = nil
+    for badword in ipairs(config.words) do
+        if string.match(string.lower(str), config.words[badword]) then
+          blacklist = true
+          word = config.words[badword]
+        else 
+            if(blacklist == true) then
+              blacklist = true
+            else 
+              blacklist = false;
+            end
+        end
+    end
+    return blacklist, word
 end
